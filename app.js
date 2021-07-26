cc51b30c7ac4c48a23ae5f3838631cc142d297ba
Yaroslav Boreyko <yaroslav.boreyko@gmail.com>
2021-07-26 21:11:36 +0300

    Stripe Webhooks implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 
@@ -49,6 +50,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in a hour!'
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout', 
+  express.raw({ type: 'application/json'}),
+  bookingController.webhookCheckout
+);
 
 app.use(express.json({ limit: '10kb'}));
 app.use(express.urlencoded({ extended: true, limit: '10kb'}));
